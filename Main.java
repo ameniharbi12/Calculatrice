7f02fc1b786d6b55b38bb004fef8e447d0f192e7
Slim <ameni@Ameni.localdomain>
2025-11-04 21:23:44 +0100

    Rajout d’un test de la vérification de la valeur de y dans la division

--- Main.java
+++ Main.java
@@ -5,15 +5,21 @@ public class Main {
         int a = 2;
         int b = 3;
 
+        // Calcul des opérations
         int resultMult = Operation.multiplication(a, b);
         int resultSub  = Operation.soustraction(a, b);
         int resultAdd  = Operation.addition(a, b);
         double resultDiv = Operation.division(a, b);
 
+        // Affichage des résultats
         System.out.println(a + " * " + b + " = " + resultMult);
         System.out.println(a + " - " + b + " = " + resultSub);
         System.out.println(a + " + " + b + " = " + resultAdd);
         System.out.println(a + " / " + b + " = " + resultDiv);
+
+        // Test de la division par zéro
+        int c = 0;
+        System.out.println(a + " / " + c + " = " + Operation.division(a, c));
     }
 }
 
