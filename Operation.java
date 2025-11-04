242bd70ac612d54b2de6d4a855a4161dcf22c233
Slim <ameni@Ameni.localdomain>
2025-11-04 19:51:26 +0100

    Création d’une opération multiplication pour un projet de calculatrice

---
+++ Operation.java
@@ -0,0 +1,10 @@
+public class Operation {
+
+    // Méthode statique pour la multiplication
+    public static int multiplication(int x, int y) {
+        System.out.println("Bonjour"); // Affiche le message
+        return x * y;
+    }
+}
+
+
