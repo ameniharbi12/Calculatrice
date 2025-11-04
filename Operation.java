0405087ec97d6a839bdc37b15c9e8a501a2e8a4b
Slim <ameni@Ameni.localdomain>
2025-11-04 21:03:01 +0100

    Ajout de la division

--- Operation.java
+++ Operation.java
@@ -1,10 +1,28 @@
 public class Operation {
 
-    // Méthode statique pour la multiplication
+    // Multiplication
     public static int multiplication(int x, int y) {
-        System.out.println("Bonjour"); // Affiche le message
+        System.out.println("Bonjour");
         return x * y;
     }
+
+    // Soustraction
+    public static int soustraction(int a, int b) {
+        return a - b;
+    }
+
+    // Addition
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+    public double division(int a, int b) {
+        if (b == 0) {
+            System.out.println("Erreur : division par zéro !");
+            return 0;
+        }
+        return (double) a / b;
+    }
 }
+
 
 
